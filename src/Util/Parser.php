8d5e0803acbd2455b99506439ea3d6ff83d36f2f
Raideer <idmania1@gmail.com>
2015-10-10 14:47:26 +0300

    Fixed the parser

--- src/Util/Parser.php
+++ src/Util/Parser.php
@@ -3,18 +3,56 @@ namespace Raideer\Tweech\Util;
 
 class Parser{
 
-  protected $regex;
+  protected $messageRegex;
+  protected $paramsRegex;
 
   public function __construct(){
-    $specialChars = preg_quote('\`_[]^|{}');
-    $lineEnd = "[\r\n]";
-    $nickname = "[A-Za-z0-9$specialChars]+";
-    $hostname = "[A-Za-z0-9\.]+";
-    $command = "[A-Za-z]+|[0-9]{3}";
-    $space = "[\s\\x00]+?";
-    $anything = "(.+)?";
-    $prefix = "(((?P<nickname>$nickname)!$nickname@$nickname\.(?P<hostname>$hostname))|(?P<servername>$hostname))";
-    $this->regex = "/^:(?P<prefix>$prefix)$space(?P<command>$command)$space:?(?P<params>$anything)?$lineEnd?/";
+
+    /**
+     * Regex for parsing the message
+     * http://tools.ietf.org/html/rfc1459#section-2.3.1
+     */
+
+    /**
+     * Examples:
+     *
+     * PREFIX                                           COM   PARAMETERS
+     *
+     * :raideeeeer!raideeeeer@raideeeeer.tmi.twitch.tv  JOIN  #lirik
+     * :raideeeeer.tmi.twitch.tv                        353   raideeeeer = #lirik :raideeeeer
+     * :raideeeeer.tmi.twitch.tv                        366   raideeeeer #lirik :End of /NAMES list
+     * :tmi.twitch.tv                                   001   raideeeeer :Welcome, GLHF!
+     * :tmi.twitch.tv                                   375   raideeeeer :-
+     *
+     */
+
+    $space = " ";
+    $null = "\\x00";
+    $crlf = "\r\n";
+    $letters = "A-Za-z";
+    $numbers = "0-9";
+    $special = preg_quote('[]\`_^{|}');
+
+    $trailing = "[^$null$crlf]*";
+    $username = "[$letters$numbers$special]+";
+    $server = "(?:(?:[$letters$numbers\.]*)\.(?:[$letters$numbers]+)\.(?:[$letters]+))";
+
+    $prefixFull = "((?P<username>$username)(!$username)?(@$username)?\.(?P<server>$server))";
+    $prefixPart = "(?:(?P<usernamep>$username)\.(?P<serverp>$server))";
+    $prefixSmall = "(?:(?P<servers>$server))";
+
+    $command = "(?P<command>[$letters]+|[$numbers]{3})";
+
+    $params = "(?P<params>$trailing)";
+
+    $prefix = "(?:$prefixFull|$prefixPart|$prefixSmall)";
+    $compiled = "(?P<prefix>:$prefix)?[$space]$command$space$params$crlf";
+
+    $this->messageRegex = "/^$compiled$/U";
+
+    $this->paramsRegex = array(
+      'PRIVMSG' => "/^(?P<channel>#$username)[$space]?:(?P<message>$trailing)$/s"
+    );
   }
 
   protected function removeIntegerKeys(array $array){
@@ -26,18 +64,51 @@ class Parser{
     return $array;
   }
 
+  protected function copyAndDelete($from,$to,$parsed){
+    if(array_key_exists($from,$parsed)){
+      if($parsed[$to] != null){
+        unset($parsed[$from]);
+        return $parsed;
+      }
+      $parsed[$to] = $parsed[$from];
+      unset($parsed[$from]);
+    }
+
+    return $parsed;
+  }
+
+  protected function parseParameters($parsed){
+    $command = strtoupper($parsed['command']);
+    if(!array_key_exists($command, $this->paramsRegex)) return $parsed;
+
+    if(!preg_match($this->paramsRegex[$command], $parsed['params'], $params)) return $parsed;
+
+    $params = $this->removeIntegerKeys($params);
+    $parsed = array_merge($parsed, $params);
+
+    return array_unique($parsed);
+  }
+
   public function parse($message){
     if(strpos($message, "\r\n") === false){
       // return null;
     }
 
-    if(!preg_match($this->regex, $message, $parsed)){
+    if(!preg_match($this->messageRegex, $message, $parsed)){
       $parsed = array('invalid' => $message);
       return $parsed;
     }
 
+    $parsed = $this->copyAndDelete('usernamep','username', $parsed);
+    $parsed = $this->copyAndDelete('serverp','server', $parsed);
+    $parsed = $this->copyAndDelete('servers','server', $parsed);
+
     $parsed['full'] = $parsed[0];
 
-    return $this->removeIntegerKeys($parsed);
+
+    $parsed = $this->parseParameters($parsed);
+
+    // return $this->removeIntegerKeys($parsed);
+    return array_filter($this->removeIntegerKeys($parsed));
   }
 }
