fcfc452064e87e88bc0f2a9d12a94ca0bb70632f
Kristofers Ozolins <es@kristofers.xyz>
2015-10-12 13:45:12 +0300

    Added starts_with and ends_with helpers

--- src/Tweech/Util/helpers.php
+++ src/Tweech/Util/helpers.php
@@ -106,3 +106,18 @@ if ( ! function_exists('with'))
 		return $object;
 	}
 }
+if ( ! function_exists('starts_with'))
+{
+	function starts_with($haystack, $needle) {
+	    // search backwards starting from haystack length characters from the end
+	    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+	}
+}
+
+if ( ! function_exists('ends_with'))
+{
+	function ends_with($haystack, $needle) {
+	    // search forward starting from end minus needle length characters
+	    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+	}
+}
