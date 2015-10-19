551cc16fb91aba6dc061d7a3015c2661eabe582a
Kristofers Ozolins <es@kristofers.xyz>
2015-10-19 10:53:21 +0300

    Started building command registry

--- src/Tweech/Command/CommandRegistry.php
+++ src/Tweech/Command/CommandRegistry.php
@@ -1,20 +1,45 @@
 <?php
 namespace Raideer\Tweech\Command;
+use Raideer\Tweech\Command\CommandInterface;
 
 class CommandRegistry{
 
-  protected $commands;
+  protected $commands = array();
+  protected $registered = array();
+  protected $registeredIdentifiers = array();
 
-  public function __construct(array $commands = array()){
+  public function register(CommandInterface $command){
+    $name = $command->getCommand();
+    if(array_key_exists($name, $this->commands)){
+      throw new \Raideer\Tweech\Exception\CommandException("Command already exists");
+    }
 
-    $this->commands = $commands;
+    $this->commands[$name] = $command;
+    $registered = array();
+    $registered[$name]['ids'] = $this->buildId($command);
+    $this->registered[] = $registered;
   }
 
-  public function registerAll(){
-
-    foreach($this->commands as $command){
-      
+  protected function buildId(CommandInterface $command){
+    $ids = array();
+    foreach($command->getCommandIdentifier() as $id){
+      $ids[] = $id . $command->getCommand();
     }
+
+    return $ids;
+  }
+
+  public function getCommandAndRegister($command){
+
+  }
+
+  public function isCommand($string){
+    foreach($this->registeredIdentifiers as $id){
+      if(starts_with($id , $string)){
+        return true;
+      }
+    }
+    return false;
   }
 
 
