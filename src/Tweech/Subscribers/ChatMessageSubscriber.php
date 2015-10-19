4dd519145bd4f2750a4353b29999b31ccf271bc3
Kristofers Ozolins <es@kristofers.xyz>
2015-10-19 10:51:38 +0300

    Registering commands

--- src/Tweech/Subscribers/ChatMessageSubscriber.php
+++ src/Tweech/Subscribers/ChatMessageSubscriber.php
@@ -1,8 +1,15 @@
 <?php
 use Raideer\Tweech\Subscribers\EventSubscriber;
 use Raideer\Tweech\Event\ChatMessageEvent;
+use Raideer\Tweech\Command\CommandRegistry;
 
 class ChatMessageSubscriber extends EventSubscriber{
+
+  protected $registry;
+
+  public function __construct(){
+    $this->registry = new CommandRegistry;
+  }
 
   public static function getSubscribedEvents()
   {
@@ -17,7 +24,10 @@ class ChatMessageSubscriber extends EventSubscriber{
 
   public function onMessageReceived(ChatMessageEvent $event)
   {
-    
+    $message = $event->getMessage();
+    if($this->registry->isCommand($message)){
+      $this->registry->getCommandAndRegister($message);
+    }
   }
 
 }
