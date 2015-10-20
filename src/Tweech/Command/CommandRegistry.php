90756c2f408edfb028ffa13e119d1063fa66f0a7
Kristofers Ozolins <es@kristofers.xyz>
2015-10-20 15:20:39 +0300

    Rewritten command registry

--- src/Tweech/Command/CommandRegistry.php
+++ src/Tweech/Command/CommandRegistry.php
@@ -4,44 +4,45 @@ use Raideer\Tweech\Command\CommandInterface;
 
 class CommandRegistry{
 
-  protected $commands = array();
-  protected $registered = array();
-  protected $registeredIdentifiers = array();
+  protected $commands = [];
+  protected $registeredIdentifiers = [];
+  protected $ids = [];
 
   public function register(CommandInterface $command){
     $name = $command->getCommand();
     if(array_key_exists($name, $this->commands)){
-      throw new \Raideer\Tweech\Exception\CommandException("Command already exists");
+      throw new CommandException("Command with name '$name' already registered");
+      return;
     }
 
+    $this->registeredIdentifiers[$name] = $ids = $command->getCommandIdentifier();
+    $this->ids = array_merge($this->ids, $ids);
     $this->commands[$name] = $command;
-    $registered = array();
-    $registered[$name]['ids'] = $ids = $this->buildId($command);
-    $this->registeredIdentifiers = array_merge($this->registeredIdentifiers, $ids);
-    $this->registered[] = $registered;
   }
 
-  protected function buildId(CommandInterface $command){
-    $ids = array();
-    foreach($command->getCommandIdentifier() as $id){
-      $ids[] = $id . $command->getCommand();
-    }
-
-    return $ids;
-  }
-
-  public function getCommandAndExecute($command){
-
-  }
-
-  public function isCommand($string){
-    foreach($this->registeredIdentifiers as $id){
-      if(starts_with($id , $string)){
-        return true;
+  public function getId($string){
+    foreach($this->ids as $id){
+      if(starts_with($string, $id)){
+        return $id;
       }
     }
-    return false;
+
+    return null;
   }
 
+  public function getCommandIfExists($string){
+    $id = $this->getId($string);
+    if($id === null){
+      return null;
+    }
 
+    foreach($this->registeredIdentifiers as $commandName => $ids){
+      if(array_search($id, $ids) !== false){
+        if(starts_with($string, $id . $commandName)){
+          return $this->commands[$commandName];
+        }
+      }
+    }
+    return null;
+  }
 }
