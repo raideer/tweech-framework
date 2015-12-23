cc359180292bd1e6489a67920fa5d90cd41a2b2e
raideer <idmania1@gmail.com>
2015-12-23 14:54:59 +0200

    Basic api implementation

--- src/Tweech/Api/Wrapper.php
+++ src/Tweech/Api/Wrapper.php
@@ -5,21 +5,63 @@ use GuzzleHttp\Client as Guzzle;
 
 class Wrapper{
 
-  protected $apiURL = "http://api.twitch.tv/kraken/";
+  protected $apiURL = "https://api.twitch.tv/kraken/";
 
   protected $client;
+  protected $resource = null;
+
+  /**
+   * API Resources
+   */
+  protected $channels;
+  protected $chat;
+  protected $follows;
 
   public function __construct(){
 
-    $this->client = new Guzzle(['base_uri' => $this->apiURL]);
+    /**
+     * Creating a Guzzle Client and setting CaCert file location for SSL verification
+     * @var GuzzleHttp\Client
+     */
+    $this->client = new Guzzle(
+      [
+        'base_uri' => $this->apiURL,
+        'verify' => realpath( __DIR__ . "/cacert.pem")
+      ]
+    );
 
+    $this->channels = new Channels($this);
+    $this->chat = new Chat($this);
+    $this->follows = new Follows($this);
+  }
+
+  public function resource($name = null){
+    if(!$name){
+      return $this->resource;
+    }
+
+    switch($name){
+      case "channels":
+        return $this->channels;
+        break;
+      case "chat":
+        return $this->chat;
+        break;
+      case "follows":
+        return $this->follows;
+        break;
+      default:
+        return null;
+        break;
+    }
   }
 
   public function get($target){
-    $response = $this->client->request('GET', $target);
+    $response = $this->client->get($target);
 
-    return $response->getBody()->getContents();
+    $body = json_decode($response->getBody()->getContents());
+
+    return (json_last_error() == JSON_ERROR_NONE)?$body:$response->getBody()->getContents();
   }
-
 
 }
