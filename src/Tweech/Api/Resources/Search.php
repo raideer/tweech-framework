1a98bf0ab9120b0ac4ce34b622eb359a5a2e276b
raideer <idmania1@gmail.com>
2015-12-24 16:53:47 +0200

    Created Search resource

---
+++ src/Tweech/Api/Resources/Search.php
@@ -0,0 +1,43 @@
+<?php
+namespace Raideer\Tweech\Api\Resources;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Search extends Resource{
+
+  public function searchChannels($query, $options = []){
+
+    $defaults = [
+      "query" => $query,
+      "limit" => 25,
+      "offset" => 0
+    ];
+
+    return $this->wrapper->get("search/channels", ['query' => $this->resolveOptions($options, $defaults)]);
+  }
+
+  public function searchStreams($query, $options = []){
+
+    $defaults = [
+      "query" => $query,
+      "limit" => 25,
+      "offset" => 0,
+      "hls" => null
+    ];
+
+    return $this->wrapper->get("search/streams", ['query' => $this->resolveOptions($options, $defaults)]);
+  }
+
+  public function searchGames($query, $options = []){
+
+    $defaults = [
+      "query" => $query,
+      "limit" => 25,
+      "type" => "suggest",
+      "live" => false
+    ];
+
+    return $this->wrapper->get("search/games", ['query' => $this->resolveOptions($options, $defaults)]);
+  }
+
+}
