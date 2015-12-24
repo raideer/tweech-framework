ea939b46ad186d8d7739018cf7895f14ae34f343
raideer <idmania1@gmail.com>
2015-12-24 17:13:52 +0200

    Added the new resources

--- src/Tweech/Api/Wrapper.php
+++ src/Tweech/Api/Wrapper.php
@@ -1,8 +1,12 @@
 <?php
 namespace Raideer\Tweech\Api;
 
+use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Client as Guzzle;
 
+/**
+ * ONLY FOR UNAUTHORIZED REQUESTS
+ */
 class Wrapper{
 
   protected $apiURL = "https://api.twitch.tv/kraken/";
@@ -16,6 +20,11 @@ class Wrapper{
   protected $channels;
   protected $chat;
   protected $follows;
+  protected $games;
+  protected $ingests;
+  protected $search;
+  protected $streams;
+  protected $teams;
 
   public function __construct(){
 
@@ -30,34 +39,62 @@ class Wrapper{
       ]
     );
 
-    $this->channels = new Channels($this);
-    $this->chat = new Chat($this);
-    $this->follows = new Follows($this);
+    $this->channels  = new Resources\Channels($this);
+    $this->chat      = new Resources\Chat($this);
+    $this->follows   = new Resources\Follows($this);
+    $this->games     = new Resources\Games($this);
+    $this->ingests   = new Resources\Ingests($this);
+    $this->search    = new Resources\Search($this);
+    $this->streams   = new Resources\Streams($this);
+    $this->teams     = new Resources\Teams($this);
   }
 
   public function resource($name = null){
-    if(!$name){
-      return $this->resource;
-    }
 
     switch($name){
       case "channels":
-        return $this->channels;
+        $this->resource = $this->channels;
         break;
+
       case "chat":
-        return $this->chat;
+        $this->resource = $this->chat;
         break;
+
       case "follows":
-        return $this->follows;
+        $this->resource = $this->follows;
         break;
-      default:
-        return null;
+
+      case "games":
+        $this->resource = $this->games;
+        break;
+
+      case "search":
+        $this->resource = $this->search;
+        break;
+
+      case "streams":
+        $this->resource = $this->streams;
+        break;
+
+      case "teams":
+        $this->resource = $this->streams;
         break;
     }
+
+    return $this->resource;
   }
 
-  public function get($target){
-    $response = $this->client->get($target);
+  public function get($target, $options = []){
+
+    try{
+      $response = $this->client->get($target, $options);
+    }catch(RequestException $e){
+      if($e->hasResponse()) {
+        $response = $e->getResponse();
+      }else{
+        return null;
+      }
+    }
 
     $body = json_decode($response->getBody()->getContents());
 
