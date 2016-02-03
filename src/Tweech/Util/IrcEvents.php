720a7afad41cad62a57ad58231d7f25964714eda
raideer <idmania1@gmail.com>
2016-02-03 17:34:17 +0200

    Added a name for 353 command

--- src/Tweech/Util/IrcEvents.php
+++ src/Tweech/Util/IrcEvents.php
@@ -3,15 +3,16 @@ namespace Raideer\Tweech\Util;
 
 class IrcEvents{
 
-  protected static $replies = array(
+  protected static $replies = [
     "001" => "RPL_WELCOME",
     "002" => "RPL_YOURHOST",
     "003" => "RPL_CREATED",
     "004" => "RPL_MYINFO",
+    "353" => "RPL_NAMREPLY",
     "372" => "RPL_MOTD",
     "375" => "RPL_MOTDSTART",
     "376" => "RPL_ENDOFMOTD"
-  );
+  ];
 
   public static function getReplies(){
     return self::$replies;
