4752bf5de63d8f4652dcecaa519799139e6d5ba7
raideer <idmania1@gmail.com>
2016-02-04 20:17:57 +0200

    Started making Emotes service

---
+++ src/Tweech/Services/Emotes.php
@@ -0,0 +1,56 @@
+<?php
+namespace Raideer\Tweech\Services;
+
+use GuzzleHttp\Client;
+
+class Emotes{
+
+  protected $globalApi = "http://twitchemotes.com/api_cache/v2/global.json";
+  protected $subscriberApi = "http://twitchemotes.com/api_cache/v2/subscriber.json";
+
+  protected $sql;
+
+  public function __construct(){
+    $this->sql = new \NoSQLite\NoSQLite('emotes.sqlite');
+  }
+
+  public function fetchGlobal(){
+
+    $json = file_get_contents($this->globalApi);
+
+    $data = json_decode($json, true);
+
+    if((json_last_error() == JSON_ERROR_NONE)){
+
+      $store = $this->sql->getStore('global');
+      $store->set('meta.generated_at', $data['meta']['generated_at']);
+      $store->set('template.small', $data['template']['small']);
+      $store->set('template.medium', $data['template']['medium']);
+      $store->set('template.large', $data['template']['large']);
+
+      foreach($data['emotes'] as $emote => $about){
+        $store->set("emote.$emote.description", $about['description']);
+        $store->set("emote.$emote.id", $about['image_id']);
+      }
+    }
+
+  }
+
+  public function queryGet($storeName, $id){
+    $store = $this->sql->getStore($storeName);
+
+    return $store->get($id);
+  }
+
+  public function fetchSubscribers(){
+
+    // return file_put_contents(
+    //   __DIR__ . "/subscribers.json",
+    //   file_get_contents($this->subscriberApi)
+    // );
+
+  }
+
+
+
+}
---
+++ tests/Services/EmotesTest.php
@@ -0,0 +1,55 @@
+<?php
+
+class EmotesTest extends PHPUnit_Framework_TestCase{
+  //
+  // private $service;
+  //
+  // public function setUp(){
+  //   $this->service = new Raideer\Tweech\Services\Emotes;
+  // }
+  //
+  // public function test_fetchGlobal_returnsArray(){
+  //   $data = $this->service->getGlobalEmotes();
+  //
+  //   $this->assertTrue(is_array($data));
+  // }
+  //
+  // public function test_fetchGlobal_expectsValidArray(){
+  //
+  //   $data = $this->service->getGlobalEmotes();
+  //
+  //   $this->assertTrue(array_key_exists("meta", $data));
+  //   $this->assertTrue(array_key_exists("template", $data));
+  //   $this->assertTrue(array_key_exists("emotes", $data));
+  // }
+  //
+  // public function test_fetchSubscribers_returnsArray(){
+  //   $data = $this->service->getSubscriberEmotes();
+  //
+  //   $this->assertTrue(is_array($data));
+  // }
+  //
+  // public function test_fetchSubscribers_expectsValidArray(){
+  //
+  //   $data = $this->service->getSubscriberEmotes();
+  //
+  //   $this->assertTrue(array_key_exists("meta", $data));
+  //   $this->assertTrue(array_key_exists("template", $data));
+  //   $this->assertTrue(array_key_exists("channels", $data));
+  // }
+  //
+  // public function test_globalIsFetched_returnsTrue(){
+  //
+  //   $this->service->getGlobalEmotes();
+  // 
+  //   $this->assertTrue($this->service->globalIsFetched());
+  // }
+  //
+  // public function test_subscribersAreFetched_returnsTrue(){
+  //
+  //   $this->service->getSubscriberEmotes();
+  //
+  //   $this->assertTrue($this->service->subscribersAreFetched());
+  // }
+
+}
