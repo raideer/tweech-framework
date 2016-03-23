c41f5d3f2d6dd9489c6cc233b1bdf14d617ed4a2
raideer <idmania1@gmail.com>
2016-03-23 18:23:44 +0200

    Fixing indentation

--- src/Tweech/Util/IrcEvents.php
+++ src/Tweech/Util/IrcEvents.php
@@ -5,15 +5,15 @@ namespace Raideer\Tweech\Util;
 class IrcEvents
 {
     protected static $replies = [
-    '001' => 'RPL_WELCOME',
-    '002' => 'RPL_YOURHOST',
-    '003' => 'RPL_CREATED',
-    '004' => 'RPL_MYINFO',
-    '353' => 'RPL_NAMREPLY',
-    '372' => 'RPL_MOTD',
-    '375' => 'RPL_MOTDSTART',
-    '376' => 'RPL_ENDOFMOTD',
-  ];
+        '001' => 'RPL_WELCOME',
+        '002' => 'RPL_YOURHOST',
+        '003' => 'RPL_CREATED',
+        '004' => 'RPL_MYINFO',
+        '353' => 'RPL_NAMREPLY',
+        '372' => 'RPL_MOTD',
+        '375' => 'RPL_MOTDSTART',
+        '376' => 'RPL_ENDOFMOTD',
+    ];
 
     public static function getReplies()
     {
@@ -26,6 +26,6 @@ class IrcEvents
             return self::$replies[$code];
         }
 
-        return;
+        return null;
     }
 }
