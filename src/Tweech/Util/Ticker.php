65effc724ae4c7ff9cfa81dd8039959d45c12dd9
raideer <idmania1@gmail.com>
2016-03-25 23:09:00 +0200

    Basic tick manager

---
+++ src/Tweech/Util/Ticker.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Raideer\Tweech\Util;
+
+use Raideer\Tweech\Client\Client;
+use Raideer\Tweech\Event\Event;
+
+class Ticker
+{
+    protected $client;
+
+    protected $lastSecond = 0;
+    protected $lastMinute = 0;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public function run()
+    {
+        $this->client->listen('tick', function(){
+            $time = microtime(true);
+
+            if ($time - $this->lastSecond >= 1) {
+                $this->lastSecond = microtime(true);
+                $this->client->dispatch('tick.second', new Event);
+            }
+
+            if ($time - $this->lastMinute >= 60) {
+                $this->lastMinute = microtime(true);
+                $this->client->dispatch('tick.minute', new Event);
+            }
+        });
+    }
+}
