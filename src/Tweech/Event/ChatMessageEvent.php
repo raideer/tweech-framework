7d9881ff29a2c9822a9712702aab8aa4c2a33e4b
raideer <idmania1@gmail.com>
2016-03-25 23:09:17 +0200

    Minor improvements

--- src/Tweech/Event/ChatMessageEvent.php
+++ src/Tweech/Event/ChatMessageEvent.php
@@ -3,19 +3,21 @@
 namespace Raideer\Tweech\Event;
 
 use Raideer\Tweech\Client\Client;
-use Raideer\Tweech\Chat\Sender;
+use Raideer\Tweech\Components\Sender;
+use Raideer\Tweech\Components\Emotes;
 
 class ChatMessageEvent extends Event
 {
     protected $response;
     protected $client;
     protected $sender;
+    protected $emotes;
+    protected $tags;
 
     public function __construct($response, Client $client)
     {
         $this->response = $response;
         $this->client = $client;
-        $this->sender = new Sender($response['tags']);
     }
 
     public function getResponse()
@@ -25,27 +27,56 @@ class ChatMessageEvent extends Event
 
     public function getSenderName()
     {
-        return $this->response['nick'];
+        return $this->getResponse()['nick'];
+    }
+
+    public function getEmotes()
+    {
+        if (!$this->emotes) {
+            $this->emotes = new Emotes($this->getTags(), $this->getMessage());
+        }
+
+        return $this->emotes;
+    }
+
+    public function getTags()
+    {
+        if (!$this->tags) {
+            $parts = explode(";", $this->getResponse()['tags']);
+            $tags = [];
+            foreach ($parts as $tag) {
+                $values = explode("=", $tag);
+                $tags[$values[0]] = $values[1];
+            }
+
+            $this->tags = $tags;
+        }
+
+        return $this->tags;
     }
 
     public function getSender()
     {
+        if (!$this->sender) {
+            $this->sender = new Sender($this->getTags());
+        }
+
         return $this->sender;
     }
 
     public function getChatName()
     {
-        return $this->response['chat'];
+        return $this->getResponse()['chat'];
     }
 
     public function getChat()
     {
-        return $this->client->getChat($this->response['chat']);
+        return $this->client->getChat($this->getChatName());
     }
 
     public function getMessage()
     {
-        return $this->response['message'];
+        return $this->getResponse()['message'];
     }
 
     public function getClient()
