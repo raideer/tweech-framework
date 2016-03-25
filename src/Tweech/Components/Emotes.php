dbdd0b0c1119b083bb59c9f43c26a450c84e3e06
raideer <idmania1@gmail.com>
2016-03-25 23:08:24 +0200

    Emote parser

--- src/Tweech/Components/Emotes.php
+++ src/Tweech/Components/Emotes.php
@@ -7,27 +7,12 @@ class Emotes
     protected $globalApi = 'http://twitchemotes.com/api_cache/v2/global.json';
     protected $subscriberApi = 'http://twitchemotes.com/api_cache/v2/subscriber.json';
 
-    protected $sql;
+    protected $tags;
+    protected $message;
 
-    public function __construct()
+    public function __construct($tags, $message)
     {
-        $this->sql = new \NoSQLite\NoSQLite('emotes.sqlite');
-    }
-
-    public function fetchGlobal()
-    {
-        $json = file_get_contents($this->globalApi);
-        flushEcho("Emotes fetched [{$this->globalApi}]");
-
-        $data = json_decode($json, true);
-
-        if ((json_last_error() == JSON_ERROR_NONE)) {
-            $store = $this->sql->getStore('global');
-
-            $store->set('meta.generated_at', $data->meta->generated_at);
-            $store->set('template.small', $data->template->small);
-            $store->set('template.medium', $data->template->medium);
-            $store->set('template.large', $data->template->large);
-        }
+        $this->tags = $tags;
+        $this->message = $message;
     }
 }
